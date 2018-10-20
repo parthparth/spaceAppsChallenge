8e3722b7090c0cce57b6560ca2f0e2226a08ca62
The Coding Aviator <34807532+thecodingaviator@users.noreply.github.com>
2018-10-20 18:41:27 +0530

    Card

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     console.clear();
+    getData();
     $('body').scrollView();
 });
 
@@ -24,6 +25,20 @@ function getData() {
             card += '<div id="launching-from" class="a-div">';
             card += '<p><strong>Launching From</strong></p>: &nbsp;';
             card += '<a href="' + element.location.pads[0].wikiURL + '" id="rocket-launch">' + element.location.name + '</a>&nbsp;|&nbsp;<a href="' + element.location.pads[0].mapURL + '" id="rocket-map">See it on a map</a>';
+            card += '</div>';
+            card += '<div id="launch-agencies">';
+            card += '<p><strong>Launch Details</strong></p>';
+            if (element.missions[0].agencies != null) {
+                element.missions[0].agencies.forEach(elements => {
+                    card += '<p> Launch Agency: '+ elements.name + '</p>';
+                });
+                card += '</div>';
+            }
+            card += element.missions[0].description;
+            if (element.missions[0].wikiURL)
+                card += '<a href=' + element.missions[0].wikiURL + '">More information</a>';
+            card += '<div id="lsp" class="a-div">';
+            card += '<p><strong>Launch Service Provider:</strong></p>&nbsp;<a href="'+element.lsp.wikiURL+'">'+element.lsp.name+'</a>';
             card += '</div>';
             card += '</div>';
             document.getElementById('cards').innerHTML += card;
